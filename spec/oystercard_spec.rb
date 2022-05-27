cdfeb0eed560871aa0bd35524e0289089bfb481b
Mark McLeod <markmcleod45@gmail.com>
2022-05-27 11:13:41 +0100

    Initialize oystercard object with a balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,11 @@ require 'oystercard'
 
 describe Oystercard do
 
+  describe '#initialize' do
+    it 'creates a oystercard object with a balance attribute of zero' do
+      expect(subject).to be_a Oystercard
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
