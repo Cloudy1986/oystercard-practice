42c9eb60b68e5337602a1d8ebff3a1037f69bea6
Mark McLeod <markmcleod45@gmail.com>
2022-05-27 12:37:04 +0100

    Refactor top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,6 +13,10 @@ describe Oystercard do
     it 'increases the balance attribute of an oystercard by the value of the argument passed to it' do
       expect{ subject.top_up(50) }.to change { subject.balance }.by 50
     end
+
+    it 'raises an error if the balance would exceed the maximum balance of 90' do
+      expect{ subject.top_up(91) }.to raise_error
+    end
   end
 
 end
